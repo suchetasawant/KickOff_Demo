cd64b9607c8e07af2e254366500984cf87e4271d
suchetasawant <suchetasawant1o91@gnail.com>
2021-06-02 19:44:01 +0530

    first kickOff code commit

--- main/java/com/KickOff/Pages/SignUpPage.java
+++
@@ -1,7 +0,0 @@
-package com.KickOff.Pages;
-
-import com.mainBaseClass.mainBaseClass;
-
-public class SignUpPage extends mainBaseClass{
-
-}
--- test/java/com/KickOffPage/test/SignUpPageTest.java
+++
@@ -1,7 +0,0 @@
-package com.KickOffPage.test;
-
-import com.mainBaseClass.mainBaseClass;
-
-public class SignUpPageTest extends mainBaseClass{
-
-}
